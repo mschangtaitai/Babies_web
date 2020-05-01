ace7e72daac7f02586301659ea90556c61d72066
Michael Chan <mschangtaitai@gmail.com>
2020-04-30 19:08:25 -0600

    migrations missing

---
+++ django_project/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\15624\Desktop\Web\Babies_web\django_project\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
